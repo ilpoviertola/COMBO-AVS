9123d45b31ce66a432aab137071e6d1002047626
Ilpo Viertola <ilpo.viertola@gmail.com>
2026-03-04 13:11:07 +0200

    test script

--- scripts/pvt_test_avms3.sh
+++ scripts/pvt_test_avms3.sh
@@ -3,7 +3,6 @@ export DETECTRON2_DATASETS=$dataset_root
 export CUDA_VISIBLE_DEVICES=1
 python pred.py \
     --num-gpus 1 \
-    --config-file configs/avs_ms3/Test_COMBO_PVTV2B5_bs8_20k.yaml \
+    --config-file configs/avs_ms3/COMBO_PVTV2B5_bs8_20k.yaml \
     --dist-url tcp://0.0.0.0:47772 \
     --eval-only \
-    
